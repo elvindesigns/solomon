a026fef1bfec777d34750a4172d4ae699a39c8ae
elvindesigns <elvindesigns@gmail.com>
2019-10-07 17:08:46 +0100

    initial push

---
+++ assets/dist/active-switcher.js
@@ -0,0 +1,35 @@
+
+var colorSheets = [
+    {
+        color: "#15db95",
+        title: "Switch to Default Color",
+        href: "./assets/css/color/default-color.css"
+    },
+    {
+        color: "#f6404f",
+        title: "Switch to #f6404f",
+        href: "./assets/css/color/color2.css"
+    },
+    {
+        color: "#30bcc9",
+        title: "Switch to #30bcc9",
+        href: "./assets/css/color/color3.css"
+    },
+    {
+        color: "#ff4800",
+        title: "Switch to #ff4800",
+        href: "./assets/css/color/color4.css"
+    },
+    {
+        color: "#f10909",
+        title: "Switch to #f10909",
+        href: "./assets/css/color/color5.css"
+    },
+    {
+        color: "#ffc815",
+        title: "Switch to #ffc815",
+        href: "./assets/css/color/color6.css"
+    }
+];
+
+ColorSwitcher.init(colorSheets);
